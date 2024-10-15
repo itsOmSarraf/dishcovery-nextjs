594e458bfb87cc8001945f354d70887e8cce7199
Om Sarraf <itsomsarraf+github@gmail.com>
2024-10-16 03:47:00 +0530

    recipe

--- app/recipe/page.tsx
+++ app/recipe/page.tsx
@@ -1,34 +1,10 @@
 'use client'
 // import { cookies } from 'next/headers';
 import RecipePage from './RecipePage';
-// import useRecipeStore from '@/lib/recipeStore';
 export default function RecipePageWrapper() {
-    // const cookieStore = cookies();
-    // const recipeCookie = cookieStore.get('generatedRecipe');
-    // const recipe = useRecipeStore();
-    // let recipeData = null;
-    // if (recipe) {
-    //     try {
-    //         recipeData = JSON.parse(recipe);
-    //     } catch (error) {
-    //         console.error('Failed to parse recipe cookie:', error);
-    //     }
-    // }
-
-    // const safeRecipeData = JSON.stringify(recipeData)
-    //     .replace(/\\/g, '\\\\')
-    //     .replace(/'/g, "\\'")
-    //     .replace(/\n/g, '\\n')
-    //     .replace(/\r/g, '\\r')
-    //     .replace(/\t/g, '\\t');
 
     return (
         <>
-            {/* <script
-                dangerouslySetInnerHTML={{
-                    __html: `window.localStorage.setItem('generatedRecipe', '${safeRecipeData}');`,
-                }}
-            /> */}
             <RecipePage />
         </>
     );
