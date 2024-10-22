c1aa57a01196c71acc1f77366130cf0097bb491b
Om Sarraf <itsomsarraf+github@gmail.com>
2024-10-22 14:33:18 +0530

    nextauth Google

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 'use client'
+import SignIn from "@/components/auth/signin";
 import { Button } from "@/components/ui/button";
 import { useRouter } from "next/navigation";
 export default function Home() {
@@ -9,6 +10,7 @@ export default function Home() {
       <div className="flex flex-col gap-4">
         <Button onClick={() => router.push('/recommend')} variant="secondary">Recommend Me</Button>
         <Button onClick={() => router.push('/gallery')} variant={'outline'} >DishGallery</Button>
+        <SignIn />
       </div>
     </div>
   );
